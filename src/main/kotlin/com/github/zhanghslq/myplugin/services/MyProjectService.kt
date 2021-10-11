5b99378edbc826d9c656b1623cdd0f84715d80f3
GitHub Action <action@github.com>
2021-10-11 09:39:12 +0000

    Template cleanup

--- src/main/kotlin/com/github/zhanghslq/myplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/zhanghslq/myplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.zhanghslq.myplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.zhanghslq.myplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
